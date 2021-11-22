44d6b0e62bf6774506cfbd6bfb322b3c9b754e2e
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-22 18:57:26 +0000

    personalized messages 2.0 - array of array

--- arrays.js
+++ arrays.js
@@ -1,8 +1,16 @@
-const generateMessages = (names) => {
-  return names.map(name => {
-    return `Hi ${name}! 50% off our best candies for you today!`;
+const generateMessages = (namesAndDiscounts) => {
+  return namesAndDiscounts.map(nameAndDiscount => {
+    const name = nameAndDiscount[0];
+    const discount = nameAndDiscount[1];
+    return `Hi ${name}! ${discount}% off our best candies for you today!`;
   });
 }
 
-const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
-console.log(generateMessages(names));
+const namesAndDiscounts = [
+  ['Anna', 50],
+  ['Laura', 40],
+  ['Josh', 30],
+  ['Min', 50],
+  ['Karla', 60]
+];
+console.log(generateMessages(namesAndDiscounts));
