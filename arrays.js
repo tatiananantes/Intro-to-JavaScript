26ecbb9621735546967c03a40bd6e0d779e7f643
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-22 18:48:04 +0000

    personalized messages 1.0 - arrays

--- arrays.js
+++ arrays.js
@@ -1,38 +1,8 @@
-// const addToBatch = (array, number) => {
-//   if (array.length <= 5) {
-//     return array.concat(number);
-//   }
-//   return array;
-// };
-
-// console.log(addToBatch([1,2,3,4], 5))
-// console.log(addToBatch([], 8))
-// console.log(addToBatch([1, 2, 3, 4, 5, 6], 7))
-
-
-const checkLength = (phoneNumber) => {
-  if (phoneNumber.length <= 10) {
-    return true;
-  } else {
-     return false;
-  }
-};
-
-console.log(checkLength('1234567890'));
-console.log(checkLength('12345678909876'));
-
-
-const filterLongNumbers = (phoneNumbers) => {
-  return phoneNumbers.filter(checkLength);
+const generateMessages = (names) => {
+  return names.map(name => {
+    return `Hi ${name}! 50% off our best candies for you today!`;
+  });
 }
 
-const phoneNumbers = [
-  '1763687364',
-  '4763687363',
-  '7867867862', 
-  'aaaaaaaabbbbbbbcccccdddddddd'
-];
-
-console.log(filterLongNumbers(phoneNumbers));
-console.log(filterLongNumbers(['4763687363', '7867867862']));
-console.log(filterLongNumbers([]));
+const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
+console.log(generateMessages(names));
