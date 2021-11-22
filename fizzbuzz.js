c66c1d69e66641108780a3ec4456c0275114d4e2
Matt Zimmer <zimmermja@gmail.com>
2021-11-22 16:21:20 +0000

    Fizzbuzz program created

---
+++ fizzbuzz.js
@@ -0,0 +1,23 @@
+// Return "Fizz" if this number is divisible by 3.
+// Return "Buzz" if this number is divisible by 5.
+// Return "FizzBuzz" if this number is divisible by both 5 and 3.
+// Otherwise, Return the number itself.
+
+const fizzBuzz = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
+};
+
+console.log(fizzBuzz(2));
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(30));
+console.log(fizzBuzz(50));
