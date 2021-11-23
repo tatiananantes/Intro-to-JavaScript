5061f56c78cf4d169cc0f26dcd0c932b1cd2a9bd
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-23 11:49:05 +0000

    search engine for candies - arrays04

--- arrays.js
+++ arrays.js
@@ -1,16 +1,18 @@
-const generateMessages = (namesAndDiscounts) => {
-  return namesAndDiscounts.map(nameAndDiscount => {
-    const name = nameAndDiscount[0];
-    const discount = nameAndDiscount[1];
-    return `Hi ${name}! ${discount}% off our best candies for you today!`;
-  });
+const candies = [['Aero', 1.99], ['Skitties', 2.99], ['Maltesers', 3.49], ['Mars', 1.49],
+ ['Skittles', 1.49], ['Starburst', 5.99], ['Ricola', 1.99], ['Polkagris', 5.99],
+ ['Pastila', 4.99], ['Mentos', 8.99], ['Raffaello', 7.99], ['Gummi bears', 10.99],
+['Fraise Tagada', 5.99]];
+
+
+const searchCandies = (searchPrefix, maxPrice) => {
+  return candies.filter(candy => {
+    return candy[0].toLowerCase().startsWith(searchPrefix.toLowerCase());
+  }).filter(candy => {
+    return candy[1] <= maxPrice;
+  }).map(candy => candy[0]);
 }
 
-const namesAndDiscounts = [
-  ['Anna', 50],
-  ['Laura', 40],
-  ['Josh', 30],
-  ['Min', 50],
-  ['Karla', 60]
-];
-console.log(generateMessages(namesAndDiscounts));
+console.log(searchCandies("Ma",10));
+console.log(searchCandies('Ma', 2));
+console.log(searchCandies('S', 10));
+console.log(searchCandies('S', 4));
