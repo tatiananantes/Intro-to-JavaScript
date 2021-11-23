5d8ee72e478fe72d621063d8cc9a2378a7b5a35c
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-23 12:01:02 +0000

    find the bug exercise - isEven/filter

--- arrays.js
+++ arrays.js
@@ -1,18 +1,11 @@
-const candies = [['Aero', 1.99], ['Skitties', 2.99], ['Maltesers', 3.49], ['Mars', 1.49],
- ['Skittles', 1.49], ['Starburst', 5.99], ['Ricola', 1.99], ['Polkagris', 5.99],
- ['Pastila', 4.99], ['Mentos', 8.99], ['Raffaello', 7.99], ['Gummi bears', 10.99],
-['Fraise Tagada', 5.99]];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
-const searchCandies = (searchPrefix, maxPrice) => {
-  return candies.filter(candy => {
-    return candy[0].toLowerCase().startsWith(searchPrefix.toLowerCase());
-  }).filter(candy => {
-    return candy[1] <= maxPrice;
-  }).map(candy => candy[0]);
+const isEvenNumber = (number) => {
+   return number % 2 == 0;
 }
 
-console.log(searchCandies("Ma",10));
-console.log(searchCandies('Ma', 2));
-console.log(searchCandies('S', 10));
-console.log(searchCandies('S', 4));
+const evenNumbers = (numbers) => {
+  return numbers.filter(isEvenNumber);
+}
+
+console.log(evenNumbers(numbers));
